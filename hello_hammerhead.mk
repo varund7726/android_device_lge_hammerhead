97c7b41a17e73fa81039cd9746a0baed06af98f6
rsbear <broderross@gmail.com>
2015-10-06 14:14:27 -0500

    Hello Marshmallow

--- hello_hammerhead.mk
+++ hello_hammerhead.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The Android Open Source Project
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
-PRODUCT_MAKEFILES := \
-		$(LOCAL_DIR)/hello_hammerhead.mk \
-    $(LOCAL_DIR)/aosp_hammerhead.mk \
-    $(LOCAL_DIR)/aosp_hammerhead_fp.mk \
-    $(LOCAL_DIR)/full_hammerhead.mk \
-    $(LOCAL_DIR)/car_hammerhead.mk
+PRODUCT_NAME := hello_hammerhead
+
+PRODUCT_PACKAGES += \
+    Launcher3
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,3 +16,4 @@
 
 add_lunch_combo aosp_hammerhead-userdebug
 add_lunch_combo aosp_hammerhead_fp-userdebug
+add_lunch_combo hello_hammerhead-userdebug
